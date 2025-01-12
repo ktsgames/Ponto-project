a3fc39b3b9d8d2796640917d6d7d39a69fc2797a
Kaique Tavares <162934069+ktsgames@users.noreply.github.com>
2025-01-12 14:48:38 -0300

    comit inicial

---
+++ src/routes/index.jsx
@@ -0,0 +1,28 @@
+
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
+
+
+import Welcome from "../pages/welcome";
+import Signin from "../pages/signin"
+
+const Stack = createNativeStackNavigator()
+
+
+export default function Routes() {
+    return (
+        <Stack.Navigator>
+            <Stack.Screen
+                name="Welcome"
+                component={Welcome}
+                options={{headerShown: false}}
+            />
+
+
+            <Stack.Screen
+                name="Signin"
+                component={Signin}
+                options={{headerShown: false}}
+            />
+        </Stack.Navigator>
+    )
+}
---
+++ styles.js
@@ -0,0 +1,11 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    textAlign: "center",
+    paddingLeft: 15,
+    paddingRight: 15,
+    paddingTop: 30,
+  }
+});
